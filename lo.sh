2f12a4c44145fbc48a1ac713e05298b0de7e603b
SysOpsXpert <sufyan_60@hotmail.com>
2025-06-24 08:47:20 +0300

    Add files via upload

---
+++ lo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the output file
+REPO_FILE="/etc/yum.repos.d/local.repo"
+
+# Create the file with the required contents
+cat <<EOF > "$REPO_FILE"
+[BaseOS]
+name=BaseOS
+baseurl=file:///cd/BaseOS
+enabled=1
+gpgcheck=0
+
+[AppStream]
+name=AppStream
+baseurl=file:///cd/AppStream
+enabled=1
+gpgcheck=0
+EOF
+
+mkdir /cd
+mount /dev/sr0 /cd
+
