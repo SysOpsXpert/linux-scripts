c935f747d6be31fae93cbf890e4369779c98659c
SysOpsXpert <sufyan_60@hotmail.com>
2025-06-24 11:26:22 +0300

    Update Docker-Install.sh

--- Docker-Install.sh
+++ Docker-Install.sh
@@ -6,4 +6,4 @@ sudo sh get-docker.sh
 #docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl enable --now docker
+sudo systemctl enable --now docker.service
