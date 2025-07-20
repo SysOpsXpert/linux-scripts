32897ffb8aecf9c5c9d847ecf0b6c2445009e555
SysOpsXpert <sufyan_60@hotmail.com>
2025-07-20 09:22:59 +0300

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+curl -fsSL setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh -o setup-repos.sh
 chmod +x setup-repos.sh
 sh setup-repos.sh
 yum install webmin -y
